c63d5eeba4e136781216257e648d036ef4b9111e
Nikita <levitinn@mail.ru>
2019-08-14 12:30:09 +0300

    firsttask

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,22 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+    let li = friends.map((item)=> {
+        // console.log(item);
+        return  Object.values(item)[0] + ' ' + Object.values(item)[1];
+
+
+    });
+
+    var ul = document.createElement('ul');
+
+    li.forEach(function(el) {
+        let listitem = document.createElement("li");
+        listitem.appendChild(document.createTextNode(el))
+        ul.appendChild(listitem);
+
+    })
+       return  document.body.appendChild(ul);
+
+
 }
